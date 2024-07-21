68654a17c83f4c40860d92daa4bfdfb864c4c90d
Diocleniax789 <julioramdom123@gmail.com>
2024-07-20 21:44:58 -0300

    se ha creado el codigo qr pero solamente los cuadros y tambien se lo ha inicializado

--- codigo_qr.pas
+++ codigo_qr.pas
@@ -7,6 +7,58 @@ TYPE
 VAR
    tab_cod_qr: tabla_codigo_qr;
 
+PROCEDURE inicializa_tabla_qr;
+VAR
+ f,j: integer;
+ BEGIN
+ FOR f:= 1 TO 21 DO
+  BEGIN
+   FOR j:= 1 TO 21 DO
+    BEGIN
+     tab_cod_qr[f,j]:= '0';
+    END;
+  END;
+ END;
+
+PROCEDURE rellena_tabla_qr;
+VAR
+ f,j: integer;
+ BEGIN
+  FOR f:= 1 TO 21 DO
+   BEGIN
+    FOR j:= 1 TO 21 DO
+     BEGIN
+     IF ((f = 1) OR ( f = 8)) AND ((j <= 8) OR ((j >= 14) AND (j <= 21))) THEN
+       tab_cod_qr[f,j]:= ' '
+     ELSE IF ((f >= 2) AND (f <= 7)) AND ( ((j = 1) OR (j = 8)) OR ((j = 14) OR (j = 21)) ) THEN
+       tab_cod_qr[f,j]:= ' '
+     ELSE IF ((f = 7) AND ((j >= 9) AND (j <= 13))) THEN
+       tab_cod_qr[f,j]:= ' '
+     ELSE IF ((f >= 9) AND (f <= 13)) AND (j = 7) THEN
+       tab_cod_qr[f,j]:= ' '
+     ELSE IF ((f = 14) OR (f = 21)) AND ((j <= 8)) THEN
+       tab_cod_qr[f,j]:= ' '
+     ELSE IF ((f >= 15) AND (f <= 20)) AND ((j = 1) OR (j = 8)) THEN
+       tab_cod_qr[f,j]:= ' ';
+    END;
+   END
+ END;
+
+PROCEDURE muestra_tabla;
+VAR
+ f,j: integer;
+ BEGIN
+ FOR f:= 1 to 21 do
+  begin
+   for j:= 1 to 21 do
+    begin
+     write(tab_cod_qr[f,j]:1);
+    end;
+    writeln();
+  end;
+ delay(90000);
+
+ END;
 
 
 PROCEDURE menu_principal;
@@ -20,15 +72,18 @@ VAR
  writeln();
  write('Seleccione opcion: ');
  readln(op);
- CASE op OF
+{ CASE op OF
       1:BEGIN
         clrscr;
         generar_codigo_qr;
         END;
- END;
+ END;               }
  UNTIL (op = 2);
  END;
 
 BEGIN
+inicializa_tabla_qr;
+rellena_tabla_qr;
+muestra_tabla;
 menu_principal;
 END.
